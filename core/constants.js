51dc1bed5835e964c6ada5e95054e35cd694e823
João Alexandre <corohsnk@hotmail.com>
2017-08-27 18:07:14 -0300

    Adicionado func items

--- core/constants.js
+++ core/constants.js
@@ -9,11 +9,18 @@
         POST_LOGIN: '/admin/auth/'
     })
     .constant('USER', {
-        GET_LIST_USER: '/admin/user/',
+        GET_LIST_USER: '/admin/user/filter/{0}',
         GET_USER: '/admin/user/{0}',
         POST_USER: '/admin/user/',
         DELETE_USER: '/admin/user/{0}',
         PUT_USER: '/admin/user/{0}'
+    })
+    .constant('ITEM', {
+        GET_LIST_ITEM: '/admin/item/filter/{0}',
+        GET_ITEM: '/admin/item/{0}',
+        POST_ITEM: '/admin/item/',
+        DELETE_ITEM: '/admin/item/{0}',
+        PUT_ITEM: '/admin/item/{0}'
     })
     .constant('STATUS', {
         GET_STATUS_SERVER: '/public/status/'
